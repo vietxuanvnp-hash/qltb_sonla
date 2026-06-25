539ffae91447fb362e06bfe060ff59e89b0db1f4
vietxuanvnp-hash <vietxuanvnp@gmail.com>
2026-06-25 10:03:32 +0700

    Update lay_thong_tin.bat

--- lay_thong_tin.bat
+++ lay_thong_tin.bat
@@ -45,6 +45,16 @@ powershell -NoProfile -ExecutionPolicy Bypass -Command ^
  "$type=if($cs.PCSystemType-eq 2){'Laptop'}else{'Desktop'};" ^
  "$pk='HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*','HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*';" ^
  "$office=(Get-ItemProperty $pk -EA SilentlyContinue|Where-Object{$_.DisplayName -match 'Microsoft 365|Microsoft Office'}|Select-Object -First 1 -ExpandProperty DisplayName);" ^
+:: ================== LẤY LICENSE ==================
+for /f "delims=" %%a in ('powershell -NoProfile -Command ^
+"Get-WmiObject -Query \"SELECT Name, LicenseStatus FROM SoftwareLicensingProduct WHERE Name like '%%Windows%%' and PartialProductKey is not null\" ^| Select-Object -First 1 LicenseStatus"') do set WIN_LICENSE=%%a
+
+for /f "delims=" %%a in ('powershell -NoProfile -Command ^
+"if (Get-Command Get-OfficeLicense -ErrorAction SilentlyContinue) { Get-OfficeLicense } else { \"Office Not Detected\" }"') do set OFFICE_LICENSE=%%a
+
+if "%WIN_LICENSE%"=="1" set WIN_LICENSE=Licensed
+if "%WIN_LICENSE%"=="0" set WIN_LICENSE=Unlicensed
+:: ================================================
  "$av=(Get-CimInstance -Namespace 'root\SecurityCenter2' -ClassName AntiVirusProduct -EA SilentlyContinue|Select-Object -ExpandProperty displayName);" ^
  "Add-Type -AssemblyName System.Web;" ^
  "function E($v){if(-not $v){return ''};[System.Web.HttpUtility]::UrlEncode($v.ToString())};" ^
