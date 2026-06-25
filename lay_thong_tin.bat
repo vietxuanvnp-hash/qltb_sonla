ebee5214a7badc9fd3cabea85fd0a86c89012ac2
VietXuan <vietxuanvp@gmail.com>
2026-06-25 21:45:04 +0700

    Trien khai repo qltb_sonla

--- lay_thong_tin.bat
+++ lay_thong_tin.bat
@@ -1,7 +1,10 @@
 @echo off
 chcp 65001 >nul
 setlocal EnableExtensions EnableDelayedExpansion
+<<<<<<< HEAD
 
+=======
+>>>>>>> c795836 (Trien khai repo qltb_sonla)
 echo Lay thong tin may tinh tu dong...
 
 set "HOSTNAME=%COMPUTERNAME%"
